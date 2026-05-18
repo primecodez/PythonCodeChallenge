b410add81165c296922b9883678056a299e1e5ff
primecodez <narendrakumarsingh2859@gmail.com>
2026-05-18 15:39:16 +0000

    Created an area calculator for circle

---
+++ challenge_2/area_of_circle.py
@@ -0,0 +1,17 @@
+radius = int(input("Enter the radius:"))
+
+r =  radius
+
+def area_circle(r):
+    return 3.14*r*r
+
+def circumference_circle(r):
+    return 2*3.14*r
+
+print("Area_odf_Circle" , area_circle(r))
+
+print("circumference_of_circle" , circumference_circle(r))
+
+
+
+
--- inner_outerloop/e2.py
+++ inner_outerloop/e2.py
@@ -1,2 +1,3 @@
+n = int(input("Enter the value:"))
 for i in range(n,n*10):
     print(i)
