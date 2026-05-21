7893a419a1837d3f3c896fdcf8efce11a4d188d0
primecodez <narendrakumarsingh2859@gmail.com>
2026-05-21 14:15:18 +0000

    time function

---
+++ Day_10/e1.py
@@ -0,0 +1,12 @@
+#Ud=sing datetime module
+
+from datetime import datetime
+
+now = datetime.now()
+year = now.year
+month = now.month
+date = now.date()
+timestamp = now.timestamp()
+
+time = now.strftime("%d %A %Y , %H:%M:%S")
+print("time:",time)
--- Day_8/h6.py
+++ Day_8/h6.py
@@ -1,5 +1,7 @@
 n = int(input("Enter the value for the pattern :"))
 lst = [(i, i**0, i**1, i**2, i**3, i**4, i**5) for i in range(n+1) ]
 
+print(lst)
+
 
 
